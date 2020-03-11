ad6875cdcc299edc1018ad25eb625615dcce1c5e
Michael <20vampir11@gmail.com>
2020-03-11 18:57:14 +0300

    finished exercise_6

--- exercise_6/src/main.f90
+++ exercise_6/src/main.f90
@@ -1,9 +1,36 @@
 program exercise_6
     implicit none
-    
+    real :: prev_z, next_z, a
+    real, parameter :: ABSERR = epsilon(1.)
+
+    print *,"Введите число, корень которого хотите вычислить"
+    read *, a
+
+    prev_z = a
+    next_z = getNextZ(prev_z, a)
+
+    do while( abs(next_z - prev_z) >= ABSERR)
+        prev_z = next_z
+        next_z = getNextZ(prev_z, a)
+        print *,prev_z
+    end do
+
+    print *,"Корень вашего числа: ", next_z
+
 contains
-    function NutonFunc(x) result (x)
-        real :: x
-        x = x*x - 
+    function getNextZ(prev_z, a) result(res)
+        real :: prev_z,a,res
+        res = prev_z - NutonFunc(prev_z, a)/NutonFuncDer(prev_z)
+    end function getNextZ
+
+    function NutonFunc(x, a) result(res)
+        real :: x, a, res
+        res = x*x - a
+    end function NutonFunc
+
+    function NutonFuncDer(x) result(res)
+        real :: x, res
+        res = x*2
+    end function NutonFuncDer
 
 end program exercise_6
