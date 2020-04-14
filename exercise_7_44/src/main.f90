81ddae778bd71d64d582a4f5d048bca107d2f885
Michael <20vampir11@gmail.com>
2020-04-14 15:25:46 +0300

    finished ex_7_44 & ex_8_12

---
+++ exercise_7_44/src/main.f90
@@ -0,0 +1,16 @@
+program exercise_7_14
+    use Environment
+
+    implicit none
+    integer, parameter :: N_ = 5
+    real :: a(N_,N_), maxvals(N_)
+    integer :: i
+
+    call random_number(a)
+    a = (a * 20) - 10
+    maxvals = maxval(a,dim=1)
+    print '('//N_//'f6.1)', (a(i, :), i=1,N_)
+    print *,"------------------"
+    print '('//N_//'f6.1)', (maxvals(i),i=1,N_)
+
+end program exercise_7_14
---
+++ exercise_8_12/src/main.f90
@@ -0,0 +1,35 @@
+program exercise_7_14
+    use Environment
+
+    implicit none
+    integer, parameter :: N_ = 10
+    real :: C(N_), D(N_)
+    integer :: i
+
+    do concurrent (i = 1:N_)
+        C(i) = i * cos(real(i))
+        D(i) = sin(real(i))**2
+    end do
+
+
+    print '('//N_//'f6.1)', (C(i), i=1,N_)
+    print *,"------------------"
+    print '('//N_//'f6.1)', (D(i), i=1,N_)
+    print *,"------------------"
+    print "(a,f5.2)","Scalar product is ",Scalar_product(C,D)
+    
+    
+contains
+
+    function Scalar_product(A, B) result(res)
+        real, intent(in) :: A(:),B(:)
+        real             :: res
+        ! integer             :: A_UBOUND = ubound(A,dim=1), B_UBOUND = ubound(B,dim=1)
+        ! if (A_UBOUND == B_UBOUND) then
+        !     res = Sum(A * B)
+        ! else
+        !     res = null
+        ! end if
+        res = sum(A*B)
+    end function Scalar_product
+end program exercise_7_14
